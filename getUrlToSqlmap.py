384eb1452c120eba11837643318627ba1f6b668d
ptbs <270354009@qq.com>
2023-04-15 02:18:54 +0800

    修正代理功能！

--- getUrlToSqlmap.py
+++ getUrlToSqlmap.py
@@ -20,7 +20,8 @@ file_path = './tmp/urls.txt'
 # 代理地址
 PROXY_HTTP = "http://127.0.0.1:8080"
 
-
+# 设置全局代理
+SET_PROXY = "set ALL_PROXY=http://127.0.0.1:8080"
 
 
 # 定义一个函数，用于验证链接是否可访问
@@ -55,7 +56,7 @@ def extract_urls(file_path, urls_file):
     url_pattern = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
     urls = re.findall(url_pattern, content)
     for url in urls:
-        if not validate_url(url):
+        if not validate_url(url):                    #确认下能否访问
             err_urls.append(url)
             urls.remove(url)
     # 匹配部分URL，并拼接成完整URL
@@ -74,7 +75,7 @@ def extract_urls(file_path, urls_file):
             else:
                 url_protocol = 'http://'
             url = url_protocol + url_host + url_path
-            if validate_url(url):
+            if validate_url(url):                        #确认下能否访问
                 urls.append(url)
             else:
                 err_urls.append(url)
@@ -92,15 +93,19 @@ def extract_urls(file_path, urls_file):
 
 def run_sqlmap(sqlmap_path, urls_file):
     # 代理地址
-    proxy_http = PROXY_HTTP
+    proxy_http = " --proxy=" + PROXY_HTTP
+
+    # 全局代理
+    set_proxy = SET_PROXY
 
     # sqlmap命令行参数
-    sqlmap_optons = " -m " + urls_file + " --random-agent -v 3 --level 5 --batch " + proxy_http
+    sqlmap_optons = " -m " + urls_file + " --random-agent -v 3 --level " + level + " --batch " + proxy_http
 
     # 构造完整的命令
     command = "python " + sqlmap_path + "\sqlmap.py" + sqlmap_optons
 
     # 执行命令
+    os.system(set_proxy)
     os.system(command)
 
 def clear():
